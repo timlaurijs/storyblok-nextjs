81a89e40132c1392b32bea75627bd9126034b152
Tim Laurijs <tim.laurijs@touchtribe.nl>
2021-06-01 09:54:28 +0200

    storyblok

---
+++ pages/api/preview.js
@@ -0,0 +1,28 @@
+export default async function preview(req, res) {
+  // Check the secret and next parameters
+  // This secret should only be known to this API route and the CMS
+  if (req.query.secret !== process.env.PREVIEW_TOKEN || !req.query.slug) {
+    return res.status(401).json({ message: 'Invalid token' })
+  }
+
+  // Enable Preview Mode by setting the cookies
+  res.setPreviewData({})
+
+  // Set cookie to None, so it can be read in the Storyblok iframe
+  const cookies = res.getHeader('Set-Cookie')
+  res.setHeader(
+    'Set-Cookie',
+    cookies.map((cookie) => cookie.replace('SameSite=Lax', 'SameSite=None')),
+  )
+
+  // Redirect to the entry location
+  let slug = req.query.slug
+
+  // Handle home slug
+  if (slug === 'home') {
+    slug = ''
+  }
+
+  // Redirect to the path from entry
+  res.redirect(`/${slug}`)
+}
